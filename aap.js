2dbf5c59e66ed48cb4669a81050def18f09a6007
unknown <you@example.com>
2022-07-15 08:33:52 -0700

    f

--- aap.js
+++ aap.js
@@ -14,234 +14,271 @@ let db;
 
 //------>>> First Route API//------>>>
 
-app.get('/', (req, res) => {
-    res.send('express server is running')
+// First route Api
+app.get('/',(req,res) => {
+    res.send(' Starbucks Node Server ')
 })
 
-//------>>> CATEGORY API//------>>>
-
-app.get('/category', (req, res) => {
-
-    let categoryId = Number(req, query.id);
-    let category_type = re.query.type;
-    let query = {};
-    if (categoryId && category_type) {
-        query = {
-            id: categoryId,
-            type: category.type
-        }
-    } else if (categoryId) {
-        query = {
-            id: categoryId
-        }
-    } else if (category_type) {
-        query = {
-            id: category_type
-        }
-    }
-    db.collection('category').find().toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    });
-})
-
-app.get('/city', (req, res) => {
-    db.collection('city').find().toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    });
-})
-
-// --------->>>>>>> API FOR GIFTCARDS <<<<<<<--------
-
-app.get('/giftCards', (req, res) => {
-    let card_id = Number(req.params.id);
-    db.collection('giftCards').find({ "gift_id": card_id }).toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    });
-})
-
-// --->>>>>> GIFT CARDS BASED ON USER'S SELECTION <<<<<<<----
-
-app.post('/giftCards', (req, res) => {
-    console, log(req.body);
-    db.collection('giftCards').find({ _id: { $in: req.body } }).toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    });
-});
-
-// -------->>>>>>>PLACING ORDER FOR GIFT CARDS <<<<<<<<-----
-
-app.post('/giftCards',(req, res)=>{
-    db.collection('giftCards').insert(req.body, (err,result)=>{
-        if(err) throw err
-        res.send(result);
-    });
-});
-
-
-// ------->>>>>>> UPDATE API FOR GIFT CARDS <<<<<<<------
-
-app.put('/updategiftOrder/:id', (req, res) => {
-    let oId = mongo.ObjectId(req.params.id)
-    let status = req.query.status ? req.query.status : 'Panding'
-
-    db.collection('giftCardOrders').updateOne({ _id: oId },
-        {
-            $set: {
-                "status": status,
-                "bank_name": req.body.bank_name,
-                "bank_status": req.body.bank_status
-            }
-        }, (err, result) => {
-            if (err) throw err
-            res.send(`status update to ${status}`);
-        })
-})
-
-// -------DELETING ORDER FOR GIFT CARDS -------
-
-app.delete('/deletegiftCardOrder', (req, res) => {
-    let email = req.params.email;
-    let query = {}
-    if (email) {
-        query = { "email": email }
-    };
-
-    db.collection('giftCards').remove(query, (req, result) => {
-        if (err) console.log(err)
-        res.send("Gift cards order deleted")
-    });
-});
-
-
-// ---------Filtering Orders --------
-
-app.get('/filter', (req, res) => {
-    let item_type = query.type;
-    let sort = { Rating: 1 };
-    let bprice = Number(req.query.bprice);
-    let aprice = Number(req.query.aprice);
-    let arate = Number(req.query.arate);
-    let brate = Number(req.query.brate);
+// Category Api
+app.get('/category',(req,res)=>{
     let categoryId = Number(req.query.id);
-    let query = {}
-    if (sort) {
-        sort = { Rating: req.query.sort };
-    }
-    if (categoryId && aprice && item_type && arate && brate) {
-        query = { _id: categoryId, $and: [{ Price: { $bprice, $it: aprice } }], type: item_type, Ratings: { $gte: arate }, Ratings: { $lte: brate } }
-    }
-    else if (categoryId && bprice && aprice) {
-        query = { _id: categoryId, $and: [{ Price: { $gt: bprice, $lt: aprice } }] }
-    }
-    else if (categoryId && item_type) {
-        query = { _id: categoryId, type: item_type }
-    }
-    else if (bprice && aprice)
+    let category_type = req.query.type;
+    let query = {};
+
+    if(categoryId && category_type){
         query = {
-            _and: [{ Price: { $gt: bprice, $lt: aprice } }]
+            id:categoryId,
+            type:category_type
         }
-    else if (arate) {
-        query = { Ratings: { $gte: arate } }
     }
-    else if (brate) {
-        query = { Ratings: { $lte: brate } }
+    else if(categoryId){
+        query={
+            id:categoryId
+        }
     }
-    if (item_type) {
-        query = { type: item_type }
+    else if(category_type){
+        query={
+            type:category_type
+        }
     }
-    db.collection('Menu').find(query).sort(sort).tiArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
-    });
-});
+    
+    db.collection('category').find(query).toArray((err,result)=>{
+        if(err) console.log(err)
+        res.send(result);
+    })
+})
 
-// ------>>>>>> PLACE ORDER FROM MENU (COFFEE/ FOOD) <<<<<<<------
+// Stores Finder Api
+app.get('/stores',(req,res)=>{
+    let city = Number(req.query.city_id);
+    let cityname = req.query.city_name;
+    let name = req.query.name;
+    let query = {};
+    if(city && cityname && name){
+        query = {
+            city_id:city, 
+            city_name:cityname
+        }
+    }
+    else if(city){
+        query = {city_id:city}
+    }
+    else if(cityname){
+        query = {city_name:cityname}
+    }
+    else if(name){
+        query = {name:name}
+    }
 
-app.post('/MenuItem',(req,res)=>{
-    console.log(req.body);
-    db.collection('Menu').find({_id:{$in:req.body}}).toArray((err,result)=>{
+    db.collection('stores').find(query).toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
-    });
-});
+    })
+})
 
-// ------>>>>>> DELETE ORDERS <<<<<<------
 
-app.delete('/deleteOrder',(req, res)=>{
-    let email=req.query.email;
-    let query = {};
-    if(email){
-        query={"email":email};
-    }
-    db.collection('orders').remove({},(err, result)=>{
-        if(err) throw err 
-        res.send("orders (s) deleted.");   
-    });
-});
-
-// --------FIND A STORE --------- 
-app.get('/store', (req, res) => {
-    let city = Number(res.query.city_id);
-    let cityName = req.query.city_Name;
-    let query = {};
-    if (city && cityName && Name) {
-        query = {
-            city_id: city,
-            city_Name: cityName
-        }
-    } else if (city) {
-        query = { city_id: city }
-    }
-    else if (cityName) {
-        query = { city_Name: cityName }
-    }
-    else if (Name) {
-        query = { Name: Name }
-    }
-
-    db.collection('store').find().toArray((err, result) => {
+app.get('/city',(req,res) => {
+    db.collection('city').find().toArray((err,result) => {
         if (err) throw err;
         res.send(result)
-    });
-});
+    })
+})
 
+app.get('/item/:id',(req,res)=>{
+    let categoryId = Number(req.params.id);
+    
+    db.collection('menu').find({"category_id":categoryId}).toArray((err,result)=>{
+        if(err) console.log(err)
+        res.send(result);
+    })
+})
 
-// ---------API FOR JOBS ---------
-app.get('/jobs', (req, res) => {
+// Api For Jobs
+app.get('/jobs',(req,res)=>{
     let cityId = Number(req.query.city_id);
     let cityname = req.query.city_name;
     let profile = req.query.profile;
+    let query = {};
     if(cityId && profile){
-        query = {city_id:city_Id,
-        profile:profile
-    };
+        query = {city_id:cityId,
+            profile:profile
+            };
     }
     else if(cityname && profile){
-        query={city_Id:city_Id};
+        query = {city_name:cityname,
+            profile:profile
+        };
     }
     else if(cityId){
-        query={city_Id:city_Id};
+        query = {city_id:cityId};
     }
     else if(cityname){
-        query={cityname:cityname};
+        query = {city_name:cityname};
     }
     else if(profile){
-        query={profile:profile}
+        query = {profile:profile};
     }
-    db.collection('jobs').find(query).toArray((err, result) => {
-         if (err) throw err;
-        res.send(result)
- });
-});
+    
+    db.collection('jobs').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
 
+
+// Gift cards
+app.get('/giftcards/:id',(req,res)=>{
+    let card_id=Number(req.params.id);
+  
+    db.collection('giftcards').find({"gift_id":card_id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);    
+    })
+})
+
+// Gift card based on user's selection
+app.post('/giftcard',(req,res)=>{
+    console.log(req.body);
+    db.collection('giftcards').find({_id:{$in:req.body}}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+// Placing Order For gift cards
+app.post('/giftcardOrder',(req,res)=>{
+    db.collection('giftcardOrders').insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send("Order for gift card successful.");
+    })
+})
+
+// Update Api for gift cards
+app.put('/updategiftOrder/:id',(req,res)=>{
+    let oId=mongo.ObjectId(req.params.id)
+
+    let status=req.query.status?req.query.status:'Pending'
+    db.collection('giftcardOrders').updateOne(
+        {_id:oId},
+        {$set:{
+                "status":status,
+                "bank_name":req.body.bank_name,
+                "bank_status":req.body.bank_status
+            
+        }},(err,result)=>{
+            if(err) throw err
+            res.send(`status updated to ${status}`);
+            
+        }
+    )
+})
+
+// Deleting Order for gift cards
+app.delete('/deletegiftOrder',(req,res)=>{
+    let email=req.query.email;
+    let query={}
+    if(email){
+        query={"email":email}
+    }
+    db.collection('giftcardOrders').remove(query,(err,result)=>{
+       if(err) console.log(err)
+       res.send("Gift card orders deleted.")
+    })
+})
+
+// Filtering Orders
+app.get('/filter',(req,res)=>{
+    let item_type=req.query.type;
+    let sort = {Ratings:1}
+    let bprice = Number(req.query.bprice);  
+    let aprice = Number(req.query.aprice);
+    let arate= Number(req.query.arate);
+    let brate= Number(req.query.brate);
+    let categoryId = Number(req.query.id);
+    let query={}
+    if(sort){
+        sort={Ratings:req.query.sort}
+    }
+    if(categoryId && bprice && aprice && item_type && arate && brate){
+        query={_id:categoryId,$and:[{Price:{$gt:bprice,$lt:aprice}}],type:item_type, Ratings:{$gte:arate}, Ratings:{$lte:brate}}
+    }
+    else if(categoryId && bprice && aprice){
+        query={_id:categoryId,$and:[{Price:{$gt:bprice,$lt:aprice}}]}
+    }
+    else if(categoryId && item_type){
+        query={_id:categoryId,type:item_type}
+    }
+    else if(bprice && aprice){
+        query={$and:[{Price:{$gt:bprice,$lt:aprice}}]}
+    }
+    else if(arate){
+        query={Ratings:{$gte:arate}}
+    }
+    else if(brate){
+        query={Ratings:{$lte:brate}}
+    }
+    if(item_type){
+        query={type:item_type}
+    }
+       
+    
+    db.collection('menu').find(query).sort(sort).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+
+})
+
+// Place order from menu (Coffee/Food)
+app.post('/placeOrder',(req,res)=>{
+    db.collection('orders').insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send("Order Added.");
+    })
+})
+
+// Menu item based on user's selection
+app.post('/menuItem',(req,res)=>{
+    console.log(req.body);
+    db.collection('menu').find({_id:{$in:req.body}}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+// Update orders
+app.put('/updateOrder/:id',(req,res) => {
+    let oId = mongo.ObjectId(req.params.id)
+    let status = req.query.status?req.query.status:'Pending'
+    db.collection('orders').updateOne(
+        {_id:oId},
+        {$set:{
+            "status":status,
+            "bank_name":req.body.bank_name,
+            "bank_status":req.body.bank_status
+        }},(err,result)=>{
+            if(err) throw err;
+            res.send(`Status Updated to ${status}`)
+        }
+    )
+})
+
+// Delete orders
+app.delete('/deleteOrder',(req,res)=>{
+    let email=req.query.email;
+    let query={}
+    if(email){
+        query={"email":email}
+    }
+    db.collection('orders').remove({},(err,result)=>{
+        if(err) throw err;
+        res.send("Order(s) deleted.");
+    })
+})
 
 // ------>>>>>> Connection with db <<<<<<--------
 
-MongoClient.connect(MongoLiveUrl, (err, client) => {
+MongoClient.connect(MongoLiveUrl , (err, client) => {
     if (err) console.log('Error While connecting');
     db = client.db('starbucksdata');
     app.listen(port, (err) => {
